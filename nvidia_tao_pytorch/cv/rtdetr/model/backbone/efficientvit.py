52872458230850193a828c42a720839dd33a3fe3
Varun Praveen <vpraveen@nvidia.com>
2024-09-24 00:46:44 +0000

    [BackBone] Unify efficientViT base backbones

--- nvidia_tao_pytorch/cv/rtdetr/model/backbone/efficientvit.py
+++ nvidia_tao_pytorch/cv/rtdetr/model/backbone/efficientvit.py
@@ -30,8 +30,10 @@ from nvidia_tao_pytorch.cv.backbone.nn.ops import (
     EfficientViTBlock
 )
 
+from nvidia_tao_pytorch.cv.backbone.efficientvit import EfficientViTBackbone
 
-class EfficientViT(nn.Module):
+
+class EfficientViT(EfficientViTBackbone):
     """EfficientViT-Base Module"""
 
     def __init__(
@@ -46,123 +48,27 @@ class EfficientViT(nn.Module):
         out_indices=[1, 2, 3],
         activation_checkpoint=False,
     ) -> None:
-        """Init function."""
-        super().__init__()
+        """Initialize an efficientViT backbone for RT-DETR."""
 
-        self.out_indices = out_indices
-
-        self.width_list = []
-        # input stem
-        self.input_stem = [
-            ConvLayer(
-                in_channels=in_channels,
-                out_channels=width_list[0],
-                stride=2,
-                norm=norm,
-                act_func=act_func,
-            )
-        ]
-        for _ in range(depth_list[0]):
-            block = self.build_local_block(
-                in_channels=width_list[0],
-                out_channels=width_list[0],
-                stride=1,
-                expand_ratio=1,
-                norm=norm,
-                act_func=act_func,
-            )
-            self.input_stem.append(ResidualBlock(block, IdentityLayer()))
-        in_channels = width_list[0]
-        self.input_stem = OpSequential(self.input_stem)
-        self.width_list.append(in_channels)
-
-        # stages
-        self.stages = []
-        for w, d in zip(width_list[1:3], depth_list[1:3]):
-            stage = []
-            for i in range(d):
-                stride = 2 if i == 0 else 1
-                block = self.build_local_block(
-                    in_channels=in_channels,
-                    out_channels=w,
-                    stride=stride,
-                    expand_ratio=expand_ratio,
-                    norm=norm,
-                    act_func=act_func,
-                )
-                block = ResidualBlock(block, IdentityLayer() if stride == 1 else None)
-                stage.append(block)
-                in_channels = w
-            self.stages.append(OpSequential(stage))
-            self.width_list.append(in_channels)
-
-        for w, d in zip(width_list[3:], depth_list[3:]):
-            stage = []
-            block = self.build_local_block(
-                in_channels=in_channels,
-                out_channels=w,
-                stride=2,
-                expand_ratio=expand_ratio,
-                norm=norm,
-                act_func=act_func,
-                fewer_norm=True,
-            )
-            stage.append(ResidualBlock(block, None))
-            in_channels = w
-
-            for _ in range(d):
-                stage.append(
-                    EfficientViTBlock(
-                        in_channels=in_channels,
-                        dim=dim,
-                        expand_ratio=expand_ratio,
-                        norm=norm,
-                        act_func=act_func,
-                    )
-                )
-            self.stages.append(OpSequential(stage))
-            self.width_list.append(in_channels)
-        self.stages = nn.ModuleList(self.stages)
+        super().__init__(
+            width_list=width_list,
+            depth_list=depth_list,
+            in_channels=in_channels,
+            dim=dim,
+            expand_ratio=expand_ratio,
+            norm=norm,
+            act_func=act_func
+        )
         self.activation_checkpoint = activation_checkpoint
         self.out_channels = width_list[2:]
+
+        self.out_indices = out_indices
 
         # add a norm layer for each output
         for i_layer in out_indices:
             layer = nn.LayerNorm(width_list[1:][i_layer])
             layer_name = f"norm{i_layer}"
             self.add_module(layer_name, layer)
-
-    @staticmethod
-    def build_local_block(
-        in_channels: int,
-        out_channels: int,
-        stride: int,
-        expand_ratio: float,
-        norm: str,
-        act_func: str,
-        fewer_norm: bool = False,
-    ) -> nn.Module:
-        """Build local block."""
-        if expand_ratio == 1:
-            block = DSConv(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                stride=stride,
-                use_bias=(True, False) if fewer_norm else False,
-                norm=(None, norm) if fewer_norm else norm,
-                act_func=(act_func, None),
-            )
-        else:
-            block = MBConv(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                stride=stride,
-                expand_ratio=expand_ratio,
-                use_bias=(True, True, False) if fewer_norm else False,
-                norm=(None, None, norm) if fewer_norm else norm,
-                act_func=(act_func, act_func, None),
-            )
-        return block
 
     def forward(self, x: torch.Tensor):
         """Forward function."""
@@ -202,7 +108,7 @@ class EfficientViTLarge(nn.Module):
         out_indices=[1, 2, 3],
         activation_checkpoint=False,
     ) -> None:
-        """Init function."""
+        """Initializing an EfficientViT-Lx series backbone for RT-DETR."""
         super().__init__()
         block_list = block_list or ["res", "fmb", "fmb", "mb", "att"]
         expand_list = expand_list or [1, 4, 4, 4, 6]
