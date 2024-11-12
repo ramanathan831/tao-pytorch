38e6cdb0f933bb19a4ee98d31d7536cc0444867e
Sean Farhat-Sabet <sfarhatsabet@nvidia.com>
2024-11-12 10:27:07 -0800

    [TLT-4076][CV] DINO configs from TAO Core

--- tests/cv_unit_test/dino/test_config.py
+++ tests/cv_unit_test/dino/test_config.py
@@ -20,10 +20,10 @@ import json
 
 from omegaconf import OmegaConf
 
-from nvidia_tao_pytorch.cv.dino.config.dataset import DINODatasetConfig, DINOAugmentationConfig
-from nvidia_tao_pytorch.cv.dino.config.model import DINOModelConfig
-from nvidia_tao_pytorch.cv.dino.config.train import DINOTrainExpConfig, OptimConfig
-from nvidia_tao_pytorch.cv.dino.config.default_config import ExperimentConfig
+from nvidia_tao_core.config.dino.dataset import DINODatasetConfig, DINOAugmentationConfig
+from nvidia_tao_core.config.dino.default_config import ExperimentConfig
+from nvidia_tao_core.config.dino.model import DINOModelConfig
+from nvidia_tao_core.config.dino.train import DINOTrainExpConfig, OptimConfig
 from nvidia_tao_pytorch.config.json_schema_validation import validate_jsonschema
 from nvidia_tao_pytorch.config.utils import (
     create_json_schema, dataclass_to_json
