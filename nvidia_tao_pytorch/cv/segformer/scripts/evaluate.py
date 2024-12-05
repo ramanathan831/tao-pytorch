9179628a72cb9599bb8b381b21494c48adc39cbb
Ramanathan Arunachalam <rarunachalam@nvidia.com>
2024-12-04 22:17:24 -0800

    [TLT-4365][Bugfix] Fix regressions from TLT-3946

--- nvidia_tao_pytorch/cv/segformer/scripts/evaluate.py
+++ nvidia_tao_pytorch/cv/segformer/scripts/evaluate.py
@@ -38,7 +38,8 @@ def run_experiment(experiment_config):
         results_dir (str): Results dir to save the evaluation result
 
     """
-    results_dir, model_path, _ = initialize_evaluation_experiment(experiment_config)
+    model_path, trainer_kwargs = initialize_evaluation_experiment(experiment_config)
+    results_dir = trainer_kwargs["default_root_dir"]
 
     mmseg_config = MMSegmentationConfig(experiment_config, phase="evaluate")
     eval_cfg = mmseg_config.updated_config
