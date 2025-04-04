960b15b72c8f225c0c7cf3c801720ee693b4f279
Varun Praveen <vpraveen@nvidia.com>
2025-04-04 17:11:39 +0000

    [TLT-4786][Bugfix] Add and update __init__.py to missing modules

--- nvidia_tao_pytorch/cv/segformer_pl/__init__.py
+++ nvidia_tao_pytorch/cv/segformer_pl/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" Init Module for SegFormer_PL. """
+"""SegFormer PyTorch Lightning module for TAO Toolkit."""
