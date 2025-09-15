838a8e35a098c80a9f08771330b5e031e62e0a91
Yu Wang <yuw@nvidia.com>
2025-09-15 11:02:34 -0700

    [TAO-622][CI] add selective tests

--- ci/run_functional_tests.py
+++ ci/run_functional_tests.py
@@ -26,7 +26,7 @@ from utils import (
 
 from utils import get_docker_command
 
-TESTS = f"pytest -v --color=yes {os.path.join(ROOT_DIR, 'tests')}" # -ss
+TESTS = f"pytest -v --color=yes --testmon {os.path.join(ROOT_DIR, 'tests')}" # -ss
 
 
 def parse_command_line(args=sys.argv[1:]):
