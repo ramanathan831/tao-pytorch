a08f9ba4d1cf7007018485b769e13f461e0388c4
Varun Praveen <vpraveen@nvidia.com>
2025-11-13 09:52:53 -0800

    [TAOVN-000][Bugfix] Update register_buffer for checkpoint saving

--- nvidia_tao_pytorch/cv/mask_grounding_dino/model/pl_gdino_model.py
+++ nvidia_tao_pytorch/cv/mask_grounding_dino/model/pl_gdino_model.py
@@ -66,7 +66,7 @@ class MaskGDINOPlModel(TAOLightningModule):
             tokenized = self.tokenizer([caption], padding="longest", return_tensors="pt")
             label_list = torch.arange(len(cap_lists))
             label_positive_map = create_positive_map(tokenized[0], label_list, cap_lists, caption)
-            self.register_buffer("label_positive_map", label_positive_map)
+            self.register_buffer("label_positive_map", label_positive_map, persistent=False)
         else:
             self.label_positive_map = None
 
